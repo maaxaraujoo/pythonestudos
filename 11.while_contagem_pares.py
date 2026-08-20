f1f74eed204d3ad2e34552394034c499e03e5b34
maaxaraujoo <max.araujo.gomes29@gmail.com>
2026-08-20 16:13:07 -0300

    Update 11.while_contagem_pares.py

--- 11.while_contagem_pares.py
+++ 11.while_contagem_pares.py
@@ -3,8 +3,8 @@ numeroPar = int(input("Digite um numero inteiro positivo:"))
 cont = 1
 quantidade = 0
 while cont <= numeroPar:
-    if cont % 2 == 0:
-        quantidade = quantidade + 1
-    cont = cont + 1 
+    if cont % 2 == 0: #se for positivo
+        quantidade = quantidade + 1 #aqui conta +1
+    cont = cont + 1 #esse contador é o nosso rotativo, para o while funcionar
 
 print(f"A quantidade de numeros pares é {quantidade}")
